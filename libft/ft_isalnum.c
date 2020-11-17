8cbc989f0228f071e442515030ea9cb9e32980a7
Yannah Brutout <ybrutout@shi-r7-p1.s19.be>
2020-11-17 10:45:46 +0100

    ft_isalnum

---
+++ libft/ft_isalnum.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isalnum.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ybrutout <ybrutout@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/17 10:41:30 by ybrutout          #+#    #+#             */
+/*   Updated: 2020/11/17 10:44:42 by ybrutout         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <ctype.h>
+#include <stdio.h>
+
+int		ft_isalnum(int c)
+{
+	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || (c >= '0' && c <= '9'))
+		return (1);
+	return (0);
+}
+
+int		main(void)
+{
+	int c;
+	int a;
+	int b;
+
+	c = 110;
+	a = 166;
+	b = 92;
+	printf("essais : %d \n", ft_isalnum(c));
+	printf("essais : %d \n", ft_isalnum(a));
+	printf("essais : %d \n", ft_isalnum(b));
+	printf("vrai : %d \n", isalnum(c));
+	printf("vrai : %d \n", isalnum(a));
+	printf("vrai : %d \n", isalnum(b));
+
+    int e;
+	int f;
+	int g;
+	int h;
+
+	e = 57;
+	f = 48;
+	g = 47;
+	h = 58;
+	printf("essais : %d \n", ft_isalnum(e));
+	printf("essais : %d \n", ft_isalnum(f));
+	printf("essais : %d \n", ft_isalnum(g));
+	printf("essais : %d \n", ft_isalnum(h));
+	printf("vrai : %d \n", isalnum(e));
+	printf("vrai : %d \n", isalnum(f));
+	printf("vrai : %d \n", isalnum(g));
+	printf("vrai : %d \n", isalnum(h));
+	return (0);
+}
--- libft/ft_isdigit.c
+++ libft/ft_isdigit.c
@@ -1,39 +1,43 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_isalpha.c                                       :+:      :+:    :+:   */
+/*   ft_isdigit.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ybrutout <ybrutout@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/11/16 17:29:55 by ybrutout          #+#    #+#             */
-/*   Updated: 2020/11/17 10:31:58 by ybrutout         ###   ########.fr       */
+/*   Created: 2020/11/17 10:28:12 by ybrutout          #+#    #+#             */
+/*   Updated: 2020/11/17 10:44:05 by ybrutout         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <ctype.h>
 #include <stdio.h>
+#include <ctype.h>
 
-int		ft_isalpha(int c)
+int		ft_isdigit(int c)
 {
-	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+	if (c >= '0' && c <= '9')
 		return (1);
 	return (0);
 }
 
 int		main(void)
 {
-	int c;
-	int a;
-	int b;
+	int e;
+	int f;
+	int g;
+	int h;
 
-	c = 110;
-	a = 166;
-	b = 92;
-	printf("essais : %d \n", ft_isalpha(c));
-	printf("essais : %d \n", ft_isalpha(a));
-	printf("essais : %d \n", ft_isalpha(b));
-	printf("vrai : %d \n", isalpha(c));
-	printf("vrai : %d \n", isalpha(a));
-	printf("vrai : %d \n", isalpha(b));
+	e = 57;
+	f = 48;
+	g = 47;
+	h = 58;
+	printf("essais : %d \n", ft_isdigit(e));
+	printf("essais : %d \n", ft_isdigit(f));
+	printf("essais : %d \n", ft_isdigit(g));
+	printf("essais : %d \n", ft_isdigit(h));
+	printf("vrai : %d \n", isdigit(e));
+	printf("vrai : %d \n", isdigit(f));
+	printf("vrai : %d \n", isdigit(g));
+	printf("vrai : %d \n", isdigit(h));
 	return (0);
 }
