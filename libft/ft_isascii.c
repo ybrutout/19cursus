926b0922c0e1f10b4ad6632758519e71263a6c87
Yannah Brutout <ybrutout@shi-r7-p1.s19.be>
2020-11-17 11:27:45 +0100

    essais

--- libft/ft_isascii.c
+++ libft/ft_isascii.c
@@ -6,7 +6,7 @@
 /*   By: ybrutout <ybrutout@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/17 10:51:50 by ybrutout          #+#    #+#             */
-/*   Updated: 2020/11/17 10:59:15 by ybrutout         ###   ########.fr       */
+/*   Updated: 2020/11/17 11:15:08 by ybrutout         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 int		ft_isascii(int c)
 {
-	if (c >= 0 && c <= 177)
+	if (c >= 0 && c <= 0177)
 		return (1);
 	return (0);
 }
@@ -27,17 +27,17 @@ int		main(void)
 	int c;
 	int d;
 
-	a = 0;
-	b = 177;
-	c = 58;
-	d = 176;
-	printf("faux : %d /n", ft_isascii(a));
-	printf("faux : %d /n", ft_isascii(b));
-	printf("faux : %d /n", ft_isascii(c));
-	printf("faux : %d /n", ft_isascii(d));
-	printf("vrai : %d /n", isascii(a));
-	printf("vrai : %d /n", isascii(b));
-	printf("vrai : %d /n", isascii(c));
-	printf("vrai : %d /n", isascii(d));
+	a = 151511561;
+	b = 0177;
+	c = 158;
+	d = 178;
+	printf("faux : %d \n", ft_isascii(a));
+	printf("faux : %d \n", ft_isascii(b));
+	printf("faux : %d \n", ft_isascii(c));
+	printf("faux : %d \n", ft_isascii(d));
+	printf("vrai : %d \n", isascii(a));
+	printf("vrai : %d \n", isascii(b));
+	printf("vrai : %d \n", isascii(c));
+	printf("vrai : %d \n", isascii(d));
 	return (0);
 }
