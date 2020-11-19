6c0645029f88b77db4ab8b0648367c6ce1e0217c
Yannah Brutout <ybrutout@mi-r3-p1.s19.be>
2020-11-19 10:10:07 +0100

    ft_memcmp

--- libft/ft_strlen.c
+++ libft/ft_strlen.c
@@ -6,20 +6,7 @@
 /*   By: ybrutout <ybrutout@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/16 15:42:48 by ybrutout          #+#    #+#             */
-/*   Updated: 2020/11/17 17:23:21 by ybrutout         ###   ########.fr       */
+/*   Updated: 2020/11/19 10:09:46 by ybrutout         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
-
-int		ft_strlen(char *str)
-{
-	long unsigned int i;
-
-	i = 0;
-	while (str[i])
-	{
-		i++;
-	}
-	return (i);
-}
