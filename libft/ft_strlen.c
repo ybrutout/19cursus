a05fbca683da880b7701c53e044f94bc030113e4
Yannah Brutout <ybrutout@mi-r3-p1.s19.be>
2020-11-19 10:22:06 +0100

    ft_strlen

--- libft/ft_strlen.c
+++ libft/ft_strlen.c
@@ -6,7 +6,32 @@
 /*   By: ybrutout <ybrutout@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/16 15:42:48 by ybrutout          #+#    #+#             */
-/*   Updated: 2020/11/19 10:09:46 by ybrutout         ###   ########.fr       */
+/*   Updated: 2020/11/19 10:21:30 by ybrutout         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <string.h>
+#include <stdio.h> 
+
+size_t		ft_strlen(const char *s)
+{
+	char	*s1;
+	size_t	i;
+
+	s1 = (char *)s;
+	i =0;
+	while (s1[i])
+	{
+		i++;
+	}
+	return (i);
+}
+
+int		main(void)
+{
+	char s[] = "\t";
+
+	printf("F longueur ; %lu\n", ft_strlen(s));
+	printf("V longueur ; %lu\n", strlen(s));
+	return(0);
+}
