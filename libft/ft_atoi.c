f9d4302d9bfb1fdc68155133fc62fc5643917a60
Yannah Brutout <yannahbrutout@MacBook-Pro-de-Yannah.local>
2020-11-23 20:27:10 +0100

    ft_atoi

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -6,19 +6,11 @@
 /*   By: yannahbrutout <yannahbrutout@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/16 15:51:40 by ybrutout          #+#    #+#             */
-/*   Updated: 2020/11/21 12:24:57 by yannahbruto      ###   ########.fr       */
+/*   Updated: 2020/11/23 19:04:01 by yannahbruto      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	wh_space(char *new_str)
-{
-	char	*i;
-
-	i = new_str;
-	while ((*i >= 9 && *i <= 13) || *i == ' ')
-		i++;
-	new_str = i;
-}
+#include "libft.h"
 
 int			ft_atoi(const char *str)
 {
@@ -29,7 +21,8 @@ int			ft_atoi(const char *str)
 	sign = 1;
 	number = 0;
 	new_str = (char *)str;
-	wh_space(new_str);
+	while ((*new_str >= 9 && *new_str <= 13) || *new_str == ' ')
+		new_str++;
 	if (*new_str == '-')
 	{	
 		sign *= -1;
