979c8a26e45657d25c66e5977c81f6b31e40d858
Yannah Brutout <yannahbrutout@MacBook-Pro-de-Yannah.local>
2020-12-06 14:00:26 +0100

    tests

--- libft-unit-test2/hardcore-mode/error.c
+++ libft-unit-test2/hardcore-mode/error.c
@@ -1,19 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putchar_fd.c                                    :+:      :+:    :+:   */
+/*   error.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yannahbrutout <yannahbrutout@student.42    +#+  +:+       +#+        */
+/*   By: inyancat <inyancat@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/11/24 18:57:26 by ybrutout          #+#    #+#             */
-/*   Updated: 2020/12/06 13:59:10 by yannahbruto      ###   ########.fr       */
+/*   Created  2016/11/11 16:47:32 by inyancat          #+#    #+#             */
+/*   Updated  2016/11/12 20:21:42 by inyancat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include "utils.h"
+#include <stdarg.h>
+extern int g_log_fd;
 
-void	ft_putchar_fd(char c, int fd)
+void error(int code, int a, const char *pattern, ...)
 {
-	if (fd > -1)
-		write(fd, &c, 1);
+	va_list	va;
+
+	va_start(va, pattern);
+	(void)a;
+	vdprintf(g_log_fd, pattern, va);
+	dprintf(g_log_fd, "\n");
+	va_end(va);
+	exit(code);
 }
