547d02444c65f73a6ca9e0a4eb41079af6af0d3c
ybrutout <brutout.yannah@gmail.com>
2022-02-16 16:10:57 +0100

    changement de test

--- Piscine_CPP/essais_cpp/Student.cpp
+++ Piscine_CPP/essais_cpp/Student.cpp
@@ -1,21 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   test.cpp                                           :+:      :+:    :+:   */
+/*   Student.cpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ybrutout <ybrutout@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/01/11 11:51:19 by ybrutout          #+#    #+#             */
-/*   Updated: 2022/02/16 16:04:20 by ybrutout         ###   ########.fr       */
+/*   Created: 2022/02/16 15:42:58 by ybrutout          #+#    #+#             */
+/*   Updated: 2022/02/16 16:06:56 by ybrutout         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Student.hpp"
 
-int    main()
+Student::Student(std::string login)
 {
-	Student  bob = Student ("bob");
-	Student  *jim = new Student("jim");
-	delete jim;
-	return (0);
+	std::cout<<"I'm create "<<login<<std::endl;
+	this->login = login;
+}
+
+Student::~Student(void)
+{
+	std::cout<<"I'm dead "<<this->login<<std::endl;
+	return ;
 }
--- Piscine_CPP/essais_cpp/Student.hpp
+++ Piscine_CPP/essais_cpp/Student.hpp
@@ -1,21 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   test.cpp                                           :+:      :+:    :+:   */
+/*   Student.hpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ybrutout <ybrutout@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/01/11 11:51:19 by ybrutout          #+#    #+#             */
-/*   Updated: 2022/02/16 16:04:20 by ybrutout         ###   ########.fr       */
+/*   Created: 2022/02/16 16:00:16 by ybrutout          #+#    #+#             */
+/*   Updated: 2022/02/16 16:04:35 by ybrutout         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Student.hpp"
+#ifndef STUDENT_HPP
+# define STUDENT_HPP
 
-int    main()
-{
-	Student  bob = Student ("bob");
-	Student  *jim = new Student("jim");
-	delete jim;
-	return (0);
-}
+# include <iostream>
+# include <string>
+
+class Student{
+	public:
+	std::string	login;
+
+	Student(std::string login);
+	~Student(void);
+};
+
+#endif
+
