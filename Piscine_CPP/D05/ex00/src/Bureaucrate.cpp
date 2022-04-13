ee2aae4148fb470b518760c43e7d97d52dbddff5
ybrutout <brutout.yannah@gmail.com>
2022-04-13 13:29:52 +0200

    ex00

--- Piscine_CPP/D05/ex00/src/Bureaucrate.cpp
+++ Piscine_CPP/D05/ex00/src/Bureaucrate.cpp
@@ -6,16 +6,13 @@
 /*   By: ybrutout <ybrutout@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/13 11:43:36 by ybrutout          #+#    #+#             */
-/*   Updated: 2022/04/13 13:18:55 by ybrutout         ###   ########.fr       */
+/*   Updated: 2022/04/13 13:29:24 by ybrutout         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/Bureaucrate.hpp"
 
-Bureaucrate::Bureaucrate() : _name("no_name") , _grade(150)
-{
-
-}
+Bureaucrate::Bureaucrate() : _name("no_name") , _grade(150){}
 
 Bureaucrate::Bureaucrate(int grade, std::string name) : _name(name)
 {
@@ -27,7 +24,35 @@ Bureaucrate::Bureaucrate(int grade, std::string name) : _name(name)
 		this->_grade = grade;
 }
 
-Bureaucrate::~Bureaucrate()
+Bureaucrate::Bureaucrate(Bureaucrate const & rhs)
 {
-	
+	this->_grade = rhs.getGrade();
+	this->_name = rhs.getName();
+}
+
+Bureaucrate::~Bureaucrate(){}
+
+Bureaucrate		&		Bureaucrate::operator=(Bureaucrate const & rhs)
+{
+	this->_grade = rhs.getGrade();
+	this->_name = rhs.getName();
+	return *this;
+}
+
+std::string				Bureaucrate::getName()const
+{
+	return this->_name;
+}
+
+int						Bureaucrate::getGrade()const
+{
+	return this->_grade;
+}
+
+void					Bureaucrate::incrementingGrade()
+{
+	if (this->_grade == 1)
+		throw Bureaucrate::GradeTooHighException();
+	else
+		this->_grade--;
 }
